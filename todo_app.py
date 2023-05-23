10e370efc239bb6dcd17425e17af0116c94bb47e
Cecile Chow <cecile.chow@montrealanalytics.com>
2023-05-22 21:23:08 -0700

    update wording

--- todo_app.py
+++ todo_app.py
@@ -11,19 +11,21 @@ def add_todo():
 
 
 st.title("My TODO App")
-st.subheader("This is a personal TODO app")
-st.write("Increase productivity")
+st.subheader("Things to do")
+st.markdown(":sunglasses: Let's cross these off!")
 
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
+        # Update todos list
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
 
 st.text_input(label="Add a new TODO:", placeholder="TODO",
               on_change=add_todo, key="new_todo")
+
 
 st.session_state
